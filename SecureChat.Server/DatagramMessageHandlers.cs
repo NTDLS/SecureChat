29691f16d844f5a681b5c6f3979612f2f5a2e4fe
Josh Patterson <Josh@NTDLS.com>
2025-03-10 00:34:49 -0400

    Start/stop

---
+++ SecureChat.Server/DatagramMessageHandlers.cs
@@ -0,0 +1,21 @@
+﻿using NTDLS.DatagramMessaging;
+
+namespace SecureChat.Server
+{
+    internal class DatagramMessageHandlers : IDmMessageHandler
+    {
+        public static void ProcessFrameNotificationCallback(DmContext context, DmNotificationBytes bytes)
+        {
+            //context.WriteReplyBytes(bytes.Bytes); //Echo the payload back to the sender.
+            //Console.WriteLine($"Received {bytes.Bytes.Length} bytes.");
+        }
+
+        /*
+        public static void ProcessFrameNotificationCallback(DmContext context, MyFirstUDPPacket payload)
+        {
+            //context.WriteReplyMessage(payload); //Echo the payload back to the sender.
+            Console.WriteLine($"{payload.Message}->{payload.UID}->{payload.TimeStamp}");
+        }
+        */
+    }
+}
