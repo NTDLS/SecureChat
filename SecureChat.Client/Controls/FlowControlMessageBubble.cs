48cc609e942b4dfeb718371f6dd6c827200ffb9c
Josh Patterson <Josh@NTDLS.com>
2025-05-13 15:24:07 -0400

    wip

--- SecureChat.Client/Controls/FlowControlMessageBubble.cs
+++ SecureChat.Client/Controls/FlowControlMessageBubble.cs
@@ -45,7 +45,6 @@ namespace SecureChat.Client.Controls
                     ForeColor = Themes.AdjustBrightness(Themes.InvertColor(_bubbleColor), 0.85f),
                     Font = font,
                     Text = displayName,
-                    Left = 0,
                     Top = 0,
                 };
                 _labelDisplayName.MouseClick += LabelMessage_MouseClick;
@@ -59,9 +58,7 @@ namespace SecureChat.Client.Controls
                 ForeColor = Themes.AdjustBrightness(Themes.InvertColor(_bubbleColor), 0.5f),
                 Font = font,
                 Text = message,
-                Left = 0,
                 Top = (_labelDisplayName?.Top + _labelDisplayName?.Height + 5) ?? 0,
-
             };
             Controls.Add(_labelMessage);
 
@@ -86,21 +83,21 @@ namespace SecureChat.Client.Controls
                 {
                     if (_labelDisplayName != null)
                     {
-                        _labelDisplayName.Padding = new Padding(padding, 0, 5, 5);
+                        _labelDisplayName.Left = padding + 10;
                     }
-                    _labelMessage.Padding = new Padding(padding, 0, 5, 5);
+                    _labelMessage.Left = padding + 10;
                 }
                 else
                 {
                     if (_labelDisplayName != null)
                     {
-                        _labelDisplayName.Padding = new Padding(5, 0, padding, 5);
+                        _labelDisplayName.Left = 10;
                     }
-                    _labelMessage.Padding = new Padding(5, 0, padding, 5);
+                    _labelMessage.Left = 10;
                 }
 
                 this.MaximumSize = new Size(_parent.Width - 30, 0);
-                _labelMessage.MaximumSize = new Size((_parent.Width - _labelMessage.Left) - 40, 0);
+                _labelMessage.MaximumSize = new Size((_parent.Width - padding) - 40, 0);
                 this.Height = _labelMessage.Top + _labelMessage.Height + 5;
                 this.Width = _labelMessage.Left + _labelMessage.Width + 5;
                 _lastWidth = _parent.Width;
@@ -112,13 +109,11 @@ namespace SecureChat.Client.Controls
             CalculateLabelSize();
 
             using var bubbleBrush = new SolidBrush(_bubbleColor);
-
-            //var rect = new Rectangle(labelMessage.Padding.Left - 5, 0, this.Width - (labelMessage.Padding.Left + labelMessage.Padding.Right) + 5, this.Height - 1);
-            //e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            //e.Graphics.FillRoundedRectangle(bubbleBrush, rect.X, rect.Y, rect.Width, rect.Height, 15);
-            //base.OnPaint(e);
+            var rect = new Rectangle(_labelMessage.Left - 5, 0, _labelMessage.Width + 5, this.Height);
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            e.Graphics.FillRoundedRectangle(bubbleBrush, rect.X, rect.Y, rect.Width, rect.Height, 15);
+            base.OnPaint(e);
         }
-
 
         private void LabelMessage_MouseClick(object? sender, MouseEventArgs e)
         {
