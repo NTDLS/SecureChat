0c4e779ca51ca7bbffed386a9631165f2b1d94f2
Josh Patterson <Josh@NTDLS.com>
2025-05-13 15:14:20 -0400

    wip.

--- SecureChat.Client/Controls/FlowControlMessageBubble.cs
+++ SecureChat.Client/Controls/FlowControlMessageBubble.cs
@@ -3,12 +3,15 @@ using static SecureChat.Library.ScConstants;
 
 namespace SecureChat.Client.Controls
 {
-    public partial class FlowControlMessageBubble : UserControl
+    public partial class FlowControlMessageBubble : Panel
     {
         private readonly Control _parent;
         private int _lastWidth = -1;
         private readonly Color _bubbleColor = Color.DodgerBlue;
         private readonly ScOrigin _origin;
+
+        private readonly Label _labelMessage;
+        private readonly Label? _labelDisplayName;
 
         public bool IsVisible =>
             _parent.ClientRectangle.IntersectsWith(_parent.RectangleToClient(Bounds));
@@ -17,8 +20,6 @@ namespace SecureChat.Client.Controls
         {
             _parent = parent;
             _origin = origin;
-
-            InitializeComponent();
 
             DoubleBuffered = true;
             SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
@@ -37,22 +38,32 @@ namespace SecureChat.Client.Controls
 
             if (showDisplayName)
             {
-                labelDisplayName.AutoSize = true;
-                labelDisplayName.BackColor = Color.Transparent;
-                labelDisplayName.ForeColor = Themes.AdjustBrightness(Themes.InvertColor(_bubbleColor), 0.85f);
-                labelDisplayName.Font = font;
-                labelDisplayName.Text = displayName;
-            }
-            else
-            {
-                labelDisplayName.Visible = false;
+                _labelDisplayName = new Label()
+                {
+                    AutoSize = true,
+                    BackColor = Color.Transparent,
+                    ForeColor = Themes.AdjustBrightness(Themes.InvertColor(_bubbleColor), 0.85f),
+                    Font = font,
+                    Text = displayName,
+                    Left = 0,
+                    Top = 0,
+                };
+                _labelDisplayName.MouseClick += LabelMessage_MouseClick;
+                Controls.Add(_labelDisplayName);
             }
 
-            labelMessage.AutoSize = true;
-            labelMessage.BackColor = Color.Transparent;
-            labelMessage.ForeColor = Themes.AdjustBrightness(Themes.InvertColor(_bubbleColor), 0.5f);
-            labelMessage.Font = font;
-            labelMessage.Text = message;
+            _labelMessage = new Label
+            {
+                AutoSize = true,
+                BackColor = Color.Transparent,
+                ForeColor = Themes.AdjustBrightness(Themes.InvertColor(_bubbleColor), 0.5f),
+                Font = font,
+                Text = message,
+                Left = 0,
+                Top = (_labelDisplayName?.Top + _labelDisplayName?.Height + 5) ?? 0,
+
+            };
+            Controls.Add(_labelMessage);
 
             CalculateLabelSize();
 
@@ -61,8 +72,7 @@ namespace SecureChat.Client.Controls
                 CalculateLabelSize();
             };
 
-            labelMessage.MouseClick += LabelMessage_MouseClick;
-            labelDisplayName.MouseClick += LabelMessage_MouseClick;
+            _labelMessage.MouseClick += LabelMessage_MouseClick;
             MouseClick += LabelMessage_MouseClick;
         }
 
@@ -74,19 +84,25 @@ namespace SecureChat.Client.Controls
 
                 if (_origin == ScOrigin.Local)
                 {
-                    labelDisplayName.Padding = new Padding(padding, 0, 5, 5);
-                    labelMessage.Padding = new Padding(padding, 0, 5, 5);
+                    if (_labelDisplayName != null)
+                    {
+                        _labelDisplayName.Padding = new Padding(padding, 0, 5, 5);
+                    }
+                    _labelMessage.Padding = new Padding(padding, 0, 5, 5);
                 }
                 else
                 {
-                    labelDisplayName.Padding = new Padding(5, 0, padding, 5);
-                    labelMessage.Padding = new Padding(5, 0, padding, 5);
+                    if (_labelDisplayName != null)
+                    {
+                        _labelDisplayName.Padding = new Padding(5, 0, padding, 5);
+                    }
+                    _labelMessage.Padding = new Padding(5, 0, padding, 5);
                 }
 
                 this.MaximumSize = new Size(_parent.Width - 30, 0);
-                labelMessage.MaximumSize = new Size((_parent.Width - labelMessage.Left) - 40, 0);
-                this.Height = labelMessage.Top + labelMessage.Height + 5;
-                this.Width = labelMessage.Left + labelMessage.Width + 5;
+                _labelMessage.MaximumSize = new Size((_parent.Width - _labelMessage.Left) - 40, 0);
+                this.Height = _labelMessage.Top + _labelMessage.Height + 5;
+                this.Width = _labelMessage.Left + _labelMessage.Width + 5;
                 _lastWidth = _parent.Width;
             }
         }
@@ -97,10 +113,10 @@ namespace SecureChat.Client.Controls
 
             using var bubbleBrush = new SolidBrush(_bubbleColor);
 
-            var rect = new Rectangle(labelMessage.Padding.Left - 5, 0, this.Width - (labelMessage.Padding.Left + labelMessage.Padding.Right) + 5, this.Height - 1);
-            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            e.Graphics.FillRoundedRectangle(bubbleBrush, rect.X, rect.Y, rect.Width, rect.Height, 15);
-            base.OnPaint(e);
+            //var rect = new Rectangle(labelMessage.Padding.Left - 5, 0, this.Width - (labelMessage.Padding.Left + labelMessage.Padding.Right) + 5, this.Height - 1);
+            //e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            //e.Graphics.FillRoundedRectangle(bubbleBrush, rect.X, rect.Y, rect.Width, rect.Height, 15);
+            //base.OnPaint(e);
         }
 
 
@@ -135,7 +151,7 @@ namespace SecureChat.Client.Controls
                 return;
             }
 
-            Exceptions.Ignore(() => Clipboard.SetText(labelMessage.Text));
+            Exceptions.Ignore(() => Clipboard.SetText(_labelMessage.Text));
         }
     }
 }
